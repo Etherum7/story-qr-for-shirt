395b1678256b045867776c6767519511b816c4cd
Etherum7 <hvs.231918@gmail.com>
2020-06-14 13:51:17 +0530

    added main paper which asks to login or signup

--- src/PreSignIn.js
+++ src/PreSignIn.js
@@ -3,53 +3,50 @@ import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 const styles = {
-    PaperDiv:{
-        display:"flex",
-        marginTop: "30vh",
-        flexDirection :"column",
-        justifyContent:"center",
-        alignItems:"center"
-
-    },
-    paperTypography:{
-        display:"block"
-    },
-    Paper:{
-        display:"flex",
-        width:"auto",
-        height:"auto",
-        flexDirection :"column",
-        justifyContent:"center",
-        paddingTop:"1.5rem"
-    },
-   
+	PaperDiv: {
+		display: 'flex',
+		marginTop: '30vh',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	paperTypography: {
+		display: 'block'
+	},
+	Paper: {
+		display: 'flex',
+		width: 'auto',
+		height: 'auto',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		padding: '1.5rem '
+	}
 };
 function PreSignIn(props) {
 	const {classes} = props;
 	return (
-        <div>
-		<div className={classes.PaperDiv}>
-			<Paper className={classes.Paper}  style={{display:"block"}}>
-                
-				<Typography variant="h2" gutterBottom>
-					Want To Get Yours ?
-				</Typography>
-                
-                
-			</Paper>
-            <Box className={classes.Paper}  style={{display:"block"}}>
-                
-				
-                
-                <div>
-                
-               
-                </div>
-			</Box>
+		<div>
+			<div className={classes.PaperDiv}>
+				<Paper className={classes.Paper} style={{display: 'block'}}>
+					<Typography variant="h2" gutterBottom>
+						Want To Get Yours ?
+					</Typography>
+					<Box>
+						<Button
+							variant="contained"
+							color="primary"
+							style={{marginRight: '0.5rem'}}>
+							LOGIN
+						</Button>
+						<Button variant="contained" color="secondary">
+							SIGN UP
+						</Button>
+					</Box>
+				</Paper>
+			</div>
 		</div>
-        </div>
 	);
 }
 
